4fc2955799a453c64f91f3bc6843ccb4613076cb
Sarah Lee <sarahselee223@gmail.com>
2021-07-18 22:19:42 -0700

    add main prototype in storyboard

---
+++ scheduler/scheduler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scheduler
+//
+//  Created by Sarah Lee on 7/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
